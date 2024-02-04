c0cc6e046d3c605f327a05ebd709f5e2e4c2e942
lurbano <lurbano@earthsciweb.org>
2024-02-04 13:49:53 -0600

    setup for Linux and PicoW servers

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # MakerspaceNetwork
+
+## server.py
+Python server for desktop (tested with Linux Mint)
+* serves (GET at "/"): ```index.py```
+
+## Makerspace POST protocol:
+* handles POST data in JSON in the form (where "lightON" is an example value).  'action' is a single string for quick messages/instructions. "value" can be any string, you just have to write the code to handle it.
+    * {'actoin': "getTime", 'value': ""}
+* POST returns the 'rData' dictionary to pass info back to the calling computer with:
+    * rData['item']
+    * rData['status']
---
+++ picoW/code.py
@@ -0,0 +1,111 @@
+
+import socketpool
+import wifi
+
+from adafruit_httpserver.mime_type import MIMEType
+from adafruit_httpserver.request import HTTPRequest
+from adafruit_httpserver.response import HTTPResponse
+from adafruit_httpserver.server import HTTPServer
+
+import json
+import board
+from digitalio import DigitalInOut, Direction
+import time
+
+with open("index.html") as f:
+    webpage = f.read()
+
+
+#ssid, password = secrets.WIFI_SSID, secrets.WIFI_PASSWORD  # pylint: disable=no-member
+ssid, password = "Wifipower, "defacto1"  # pylint: disable=no-member
+
+print("Connecting to", ssid)
+wifi.radio.connect(ssid, password)
+print("Connected to", ssid)
+
+pool = socketpool.SocketPool(wifi.radio)
+server = HTTPServer(pool)
+
+
+led = DigitalInOut(board.LED)
+led.direction = Direction.OUTPUT
+led.value = False
+
+
+
+def requestToArray(request):
+    raw_text = request.body.decode("utf8")
+    print("Raw")
+    data = json.loads(raw_text)
+    return data
+
+@server.route("/", "GET")
+def base(request: HTTPRequest):
+    """
+    Serve the default index.html file.
+    """
+    with HTTPResponse(request, content_type=MIMEType.TYPE_HTML) as response:
+        #response.send(f"{webpage()}")
+        response.send(webpage)
+
+@server.route("/", "POST")
+def base(request: HTTPRequest):
+    """
+    Serve the default index.html file.
+    """
+    rData = {}
+        
+    print("POST")
+    data = requestToArray(request)
+    print(f"data: {data} ")
+    print(f"action: {data['action']} & value: {data['value']}")
+
+    # SET MODE
+    if (data['action'] == "lightON"):
+        led.value = True
+    if (data['action'] == "lightOFF"):
+        led.value = False
+
+        rData['item'] = "onboardLED"
+        rData['status'] = led.value
+
+
+
+@server.route("/led", "POST")
+def ledButton(request: HTTPRequest):
+    # data = json.loads(raw_text)
+    data = requestToArray(request)
+    print(f"data: {data} & action: {data['action']}")
+    rData = {}
+    
+    if (data['action'] == 'lightON'):
+        led.value = True
+        
+    if (data['action'] == 'lightOFF'):
+        led.value = False
+    
+    rData['item'] = "onboardLED"
+    rData['status'] = led.value
+        
+    with HTTPResponse(request) as response:
+        response.send(json.dumps(rData))
+
+ 
+print(f"Listening on http://{wifi.radio.ipv4_address}:80")
+# Start the server.
+server.start(str(wifi.radio.ipv4_address))
+
+while True:
+    try:
+        # Process any waiting requests
+        server.poll()
+        time.sleep(0.1)
+    except OSError as error:
+        print(error)
+        continue
+
+        
+
+
+
+
