9a6c8207e0a62e64ec3896f701d17f3d2c62ae82
lurbano <lurbano@earthsciweb.org>
2024-08-09 14:32:39 -0500

    picoHexW

---
+++ examples/picoW/picoHexW/code_basic.py
@@ -0,0 +1,23 @@
+import time
+
+from cHex import *
+#h1 = hex(2)
+#h1.top((255,255,0))
+#h1.bot((255,255,255))
+#for  i in range(19):
+#	hexes[i].top((255,0,0))
+#	time.sleep(.1)
+#for i in range(19):
+#	hexes[i].top((0,255,0))
+#	time.sleep(.1)
+#
+#print(hexes[5].i,hexes[5].j)
+
+ts = tileset()
+#xts.hexes[10].bot((0,255,0))
+#ts.makeRed(1)
+#ts.makeColor((0,0,255))
+ts.firstCircle((0,0,200))
+ts.secondRing((0,100,0))
+ts.lastRing((100,0,0))
+#ts.infinite(5)
