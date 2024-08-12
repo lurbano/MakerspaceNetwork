150911e1f19e0d816e76dccdc3bc73fec27542a4
lurbano <lurbano@earthsciweb.org>
2024-08-12 18:03:27 -0500

    cpy v9.11 update

---
+++ baseStation/uDb.py
@@ -0,0 +1,30 @@
+
+from tinydb import TinyDB, Query
+
+db_path = './db/'
+
+class uDb:
+    def __init__(self):
+        self.activeDb = TinyDB(db_path+'activeDb.json')
+
+    def insert(self, 
+               ip="", 
+               hostname="", 
+               job="", 
+               notes=""):
+        id = self.activeDb.insert({'ip': ip, 'hostname': hostname, 'job': job, 'notes': notes})
+        return id
+    
+    def find(self, param="", value=""):
+        q = Query()
+        result = self.activeDb.search(q[param] == value)
+        return result
+
+
+# testing
+if __name__ == '__main__':
+    db = uDb()
+    id = db.insert(ip = '20.0.0.1:80', hostname='makerspace.local', job='Base Station', notes='Base Station')
+    id = db.insert(ip = '20.0.0.2:80', hostname='photoResistor.local', job='Makerspace Photoresistor', notes='Photo resistor that monitors light levels in the Makerspace')
+    result = db.find('job', "Base Station")
+    print(result)
