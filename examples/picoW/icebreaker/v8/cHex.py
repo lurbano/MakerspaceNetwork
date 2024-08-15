41d694ede336bd4fe2b4b4696ae0529fc38c4639
lurbano <lurbano@earthsciweb.org>
2024-08-15 11:49:16 -0500

    update

---
+++ examples/picoW/icebreaker/v8/cHex.py
@@ -0,0 +1,58 @@
+import board 
+import time
+import neopixel
+npix = 38 
+
+pixels = neopixel.NeoPixel(board.GP27, npix)
+class hex:
+	def __init__(self,n): #n is the number of the first 2 pixels for this hex.
+		self.i=n
+		self.j=n+1
+	def top(self,color=(255,255,255)):
+		pixels[self.i]=color
+		print(color)
+	def bot(self,color=(255,255,255)):
+		pixels[self.j]=color
+	def color(self,color=(255,255,255)):
+		pixels[self.i]=color
+		pixels[self.j]=color
+#hexes = []
+#for i in range(19):
+#	hexes.append(hex(i*2))
+
+class tileset:
+	def __init__(self):
+		self.hexes = []
+		for i in range(19):
+			self.hexes.append(hex(i*2))
+	def makeRed(self,dt=.5):
+		for h in self.hexes:
+			h.color((255,0,0))
+			time.sleep(dt)
+	def makeColor(self,cl=(0,255,0),dt=.1):
+		for h in self.hexes:
+			h.color(cl)
+			time.sleep(dt)
+	def firstCircle(self,col=(255,0,0)):
+		self.hexes[0].color(col)
+	def secondRing(self,col=(255,255,255)):
+		for i in range(1,7):
+			self.hexes[i].color(col)
+	def lastRing(self,col=(0,0,255)):
+		for i in range(7,19):
+			self.hexes[i].color(col)
+	def off(self,cl=(0,0,0)):
+		for h in self.hexes:
+			h.color(cl)
+	def repeat(self,n=10,dt=.25):
+		for i in range(n):
+			self.firstCircle()
+			time.sleep(dt)
+			self.secondRing()
+			time.sleep(dt)
+			self.lastRing()
+			time.sleep(dt)
+			self.off()
+	def infinite(self,dt=.25):
+		while True:
+			self.repeat(n=1,dt=dt)
---
+++ examples/picoW/icebreaker/v9.11/code.py
@@ -0,0 +1,176 @@
+
+import socketpool
+import wifi
+
+from adafruit_httpserver import Server, Request, Response, POST
+
+import json
+import board
+from digitalio import DigitalInOut, Direction
+import time
+
+from cHex import *
+ts = tileset()
+lightSwitch = "on"
+lightSwitchOld = "off"
+
+# SET UP NETWORK
+from uNetComm import *
+deviceInfo = {
+    'deviceName': 'picow_netTester',
+    'notes': 'picoW used to test the Makerspace Network',
+    'hostname': ''
+    }
+
+#  connect to network
+print()
+print("Connecting to WiFi")
+#  connect to your SSID
+wifi.radio.connect('Wifipower', 'defacto1')
+
+with open("index.html") as f:
+    webpage = f.read()
+
+print("Connected to WiFi")
+pool = socketpool.SocketPool(wifi.radio)
+server = Server(pool, "/static", debug=True)
+port = 80
+comm = uNetComm(pool)
+#/ NETWORK 
+
+
+#  onboard LED setup
+led = DigitalInOut(board.LED)
+led.direction = Direction.OUTPUT
+led.value = False
+
+
+
+def requestToArray(request):
+    raw_text = request.body.decode("utf8")
+    print("Raw")
+    try:
+        data = json.loads(raw_text)
+    except:
+        print()
+        print("Unable to convert request to object: requestToArray()")
+        print()
+        data = {}
+        data["action"] = ""
+        data["value"] = ""
+    return data
+
+@server.route("/", "GET")
+def base(request: Request):
+    """
+    Serve the default index.html file.
+    """
+    return Response(request, f"{webpage}", content_type='text/html')
+
+
+@server.route("/", "POST")
+def base(request: Request):
+    """
+    Serve the default index.html file.
+    """
+    rData = {}
+        
+    print("POST")
+    data = requestToArray(request)
+    print(f"data: {data} ")
+    print(f"action: {data['action']} & value: {data['value']}")
+
+    # SET MODE
+    if (data['action'] == "lightON"):
+        led.value = True
+        rData['item'] = "onboardLED"
+        rData['status'] = led.value
+    if (data['action'] == "lightOFF"):
+        led.value = False
+
+        rData['item'] = "onboardLED"
+        rData['status'] = led.value
+        
+    if (data['action'] == "hexOn"):
+        global lightSwitch
+        lightSwitch = 'on'
+
+        rData['item'] = "hexLight"
+        rData['status'] = lightSwitch
+        
+    if (data['action'] == "hexOff"):
+        global lightSwitch
+        lightSwitch = 'off'
+
+        rData['item'] = "hexLight"
+        rData['status'] = lightSwitch
+
+    return Response(request, json.dumps(rData))
+
+@server.route("/led", "GET")
+def ledButton(request: HTTPRequest):
+    rData = {}
+    
+    if led.value:
+        led.value = False
+    else:
+        led.value = True
+    
+    rData['item'] = "onboardLED"
+    rData['status'] = led.value
+        
+    return Response(request, json.dumps(rData))
+
+ 
+# STARTING SERVER
+print("starting server..")
+# startup the server
+try:
+    server.start(str(wifi.radio.ipv4_address), port)
+    print(f"Listening on http://{wifi.radio.ipv4_address}:{port}" )
+    # log device on makerspace network
+    regInfo = {"ip": f'{wifi.radio.ipv4_address}:{port}',
+               "deviceName": deviceInfo['deviceName'],
+               "hostname": deviceInfo['hostname'],
+               "notes": deviceInfo['notes']
+               }
+    regData = comm.request("http://makerspace.local:27182", "registerDevice", regInfo)
+    print('registered:', regData.text)
+        
+
+#  if the server fails to begin, restart the pico w
+except OSError:
+    time.sleep(5)
+    print("restarting..")
+    microcontroller.reset()
+#/ STARTING SERVER
+
+
+while True:
+    try:
+        # Process any waiting requests
+        server.poll()
+        #print(lightSwitch)
+        if (lightSwitch != lightSwitchOld):
+            
+            lightSwitchOld = lightSwitch
+            
+            if lightSwitch == 'on':
+                ts.firstCircle((0,0,200))
+                ts.secondRing((0,100,0))
+                ts.lastRing((100,0,0))
+            elif lightSwitch == 'off':
+                ts.firstCircle((0,0,0))
+                ts.secondRing((0,0,0))
+                ts.lastRing((0,0,0))
+        time.sleep(0.1)
+        
+    except OSError as error:
+        print(error)
+        continue
+
+        
+
+
+
+
